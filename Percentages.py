e0aeedf4a5e8de00fcd6dfd33a491b5f33e5fe45
Tani-alt <Tanirika.gupta@gmail.com>
2025-01-06 21:54:21 +0530

    Percentage.py

---
+++ Percentages.py
@@ -0,0 +1,12 @@
+#Printpercentage//
+English=8
+Math=10
+Science=9
+Hindi=10
+Sum=English+Hindi+Math+Science
+print("The sum of given numbers is",Sum)
+Average=Sum/4
+print("The average of number is", Average)
+Percentage=(Sum/40)*100
+print ("My percentage is",Percentage)
+
