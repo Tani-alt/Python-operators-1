7046558ed745489da768ac2c2c46851832b97c92
Tani-alt <Tanirika.gupta@gmail.com>
2025-01-06 21:44:03 +0530

    Count the notes

---
+++ Count the notes .py
@@ -0,0 +1,9 @@
+Amount=int(input("Enter the value of amount"))
+note_1=(Amount//100)
+note_2=(Amount%100)//50
+note_3=((Amount%100)%50)//10
+print("Number of 100 notes ",note_1)
+print("Number of 50 notes ",note_2)
+print("Number of 10 notes ",note_3)
+
+           
